77b1ceca759c15a5cbf26bd51d80f0fe2e18b78d
Jérôme Schneider <jschneider@jsmail.fr>
2018-01-26 18:40:26 +0100

    add support for bootstrap and registration

--- hkis/settings.py
+++ hkis/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'website',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django registration
+ACCOUNT_ACTIVATION_DAYS = 7
