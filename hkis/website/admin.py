a1bb95138f9d6b86f46eb74c83781eaa0c855af9
Jérôme Schneider <jschneider@jsmail.fr>
2018-01-26 16:59:34 +0100

    Initial commit

---
+++ hkis/website/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from website.models import Answer, Exercise
+
+admin.site.register(Answer)
+admin.site.register(Exercise)
---
+++ hkis/website/models.py
@@ -0,0 +1,21 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=255)
+    check = models.TextField()
+    solution = models.TextField()
+
+class Answer(models.Model):
+    exercise = models.ForeignKey(Exercise,
+            on_delete=models.CASCADE)
+    user = models.ForeignKey(User,
+            on_delete=models.CASCADE)
+    source_code = models.TextField()
+    is_corrected = models.BooleanField()
+    is_valid = models.BooleanField()
+    correction_message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    corrected_at = models.DateTimeField()
