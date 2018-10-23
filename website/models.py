86621892278c67fc32b24a408f843086572ebf17
Julien Palard <julien@palard.fr>
2018-10-23 23:42:04 +0200

    Mandatory black, pylint, mypy, flake8 before editing þ

--- website/models.py
+++ website/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from notifications.signals import notify
 from django.db.models.signals import post_save
+from notifications.signals import notify
+
 
 class Exercise(models.Model):
     title = models.CharField(max_length=255)
@@ -16,29 +17,31 @@ class Exercise(models.Model):
     def __str__(self):
         return self.title
 
+
 class Answer(models.Model):
-    exercise = models.ForeignKey(Exercise,
-            on_delete=models.CASCADE,
-            related_name="answers")
-    user = models.ForeignKey(User,
-            on_delete=models.CASCADE,
-            editable=False)
+    exercise = models.ForeignKey(
+        Exercise, on_delete=models.CASCADE, related_name="answers"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     source_code = models.TextField()
     is_corrected = models.BooleanField(default=False)
     is_valid = models.BooleanField(default=False)
-    correction_message = models.TextField(default='', blank=True)
+    correction_message = models.TextField(default="", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     corrected_at = models.DateTimeField(blank=True, null=True)
 
     def get_absolute_url(self):
-        return reverse('exercise', args=[self.exercise.id])
+        return reverse("exercise", args=[self.exercise.id])
 
 
-def new_answer(sender, instance, created, **kwargs):
+def cb_new_answer(sender, instance, created, **kwargs):
     if instance.is_corrected:
-        notify.send(instance,
-                recipient=instance.user,
-                verb="correction",
-                description=instance.correction_message)
+        notify.send(
+            instance,
+            recipient=instance.user,
+            verb="correction",
+            description=instance.correction_message,
+        )
 
-post_save.connect(new_answer, sender=Answer)
+
+post_save.connect(cb_new_answer, sender=Answer)
