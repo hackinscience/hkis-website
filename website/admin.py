caa6098dba5841b8aedb07ee2dacd3abef7ea5f3
Julien Palard <julien@palard.fr>
2018-11-29 02:16:20 +0100

    Use AceWidget in admin too.

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,31 @@
+from django import forms
 from django.contrib import admin
+from django_ace import AceWidget
 from website.models import Answer, Exercise
+from website.forms import AnswerForm
+
+
+class AdminExerciseForm(forms.ModelForm):
+    class Meta:
+        model = Exercise
+        fields = "__all__"
+        widgets = {
+            "solution": AceWidget(
+                mode="python", theme="twilight", width="800px", height="400px"
+            ),
+            "check": AceWidget(
+                mode="python", theme="twilight", width="800px", height="400px"
+            ),
+            "wording": AceWidget(
+                mode="markdown", theme="twilight", width="800px", height="400px"
+            ),
+        }
 
 
 class ExerciseAdmin(admin.ModelAdmin):
     readonly_fields = ("id",)
     list_display = ("position", "title")
+    form = AdminExerciseForm
 
 
 class AnswerAdmin(admin.ModelAdmin):
