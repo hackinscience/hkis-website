5bdccdd58c193cf16c2da9808c35eb1d327d5736
Julien Palard <julien@palard.fr>
2021-02-09 16:59:03 +0100

    Add missing as_asgi for channels 3.

--- website/routing.py
+++ website/routing.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r"^ws/exercises/(?P<exercise_id>[0-9]+)/", consumers.ExerciseConsumer),
+    url(
+        r"^ws/exercises/(?P<exercise_id>[0-9]+)/", consumers.ExerciseConsumer.as_asgi()
+    ),
 ]
