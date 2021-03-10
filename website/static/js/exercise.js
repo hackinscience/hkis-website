ab4bf67526d29851b77f7c1c8b0f12666692796f
Julien Palard <julien@palard.fr>
2021-03-10 11:00:28 +0100

    Don't tell user to login ATM, it's useless.

--- website/static/js/exercise.js
+++ website/static/js/exercise.js
@@ -64,7 +64,7 @@ function fill_answer(answer) {
             if (!answer.user) {
                 div.appendChild(hkis.createElement("div", {
                     className: "alert alert-warning",
-                    innerHTML: interpolate(gettext('<a href="%s">Login</a> to backup your code and progression.'), [HKIS_SETTINGS.AUTH_LOGIN_URL])
+                    innerHTML: '(Not saved, see "Our DC burned down" page)'
                 }));
             }
             document.getElementById("solution_link").classList.remove("disabled");
