a7c7238df719a50c59624502d6ed69dcc2d50167
Julien Palard <julien@palard.fr>
2021-05-07 18:32:04 +0200

    url is deprecated in favor of re_path.

--- website/routing.py
+++ website/routing.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    url(
+    re_path(
         r"^ws/exercises/(?P<exercise_id>[0-9]+)/", consumers.ExerciseConsumer.as_asgi()
     ),
 ]
