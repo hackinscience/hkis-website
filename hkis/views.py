1b531875549b5fd6ab30adaecd506a2923dbfcff
Julien Palard <julien@palard.fr>
2021-11-28 20:38:25 +0100

    Exclude superusers from leaderboard, I'm always at the first place just because I test non-public exercises...

--- hkis/views.py
+++ hkis/views.py
@@ -70,7 +70,9 @@ class ProfileView(LoginRequiredMixin, UpdateView):
 
 
 class Leaderboard(ListView):
-    queryset = User.objects.filter(hkis__isnull=False).select_related("hkis")
+    queryset = User.objects.filter(
+        hkis__isnull=False, is_superuser=False
+    ).select_related("hkis")
     paginate_by = 100
     template_name = "hkis/leaderboard.html"
     ordering = ["-hkis__points"]
